963c1677eee18fe9b1070cf772fc5dc4af531b2a
BrittanyNovak <brittanynicole0106@gmail.com>
2015-10-20 18:44:59 -0500

    even numbers quiz

---
+++ EvenNumQuiz.java
@@ -0,0 +1,22 @@
+class EvenNumQuiz{
+
+	public static void main(String[] args){
+	
+		int limit = 1000;
+		
+		System.out.println("Print even numbers between 50 and 1000");
+		
+		for(int i=50; i <=limit; i++){
+			if(i%2 == 0){
+				System.out.print(i + ",");
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
